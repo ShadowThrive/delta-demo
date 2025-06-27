9c2b2fd3d5b2b132646f8d20964b3e69d20743c7
user.email <riteshstudyonly360@gmail.com>
2025-06-27 19:43:34 +0530

    modifying html and js file

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 let p=document.querySelector("p");
 p.addEventListener("click",()=>{
-    return final()
+     final();
         });
 function cchange(col,delay,next){
 setTimeout(()=>{
     p.style.color=col;
-    next()
-},delay,)
+    if(typeof next==="function"){
+    next();
+    }
+},delay)
 }
 function final(){cchange("green",1000,()=>{
     cchange("blue",1000,()=>{
         cchange("pink",1000,()=>{
             cchange("yellow",1000,()=>{
                 cchange("yellowgreen",1000);
-            })
-        })
-    })
-})
+            });
+        });
+    });
+});
 }
